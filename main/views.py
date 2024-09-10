17cb03ae8f11489412c5654a24f127ea5efca583
F00-Fighter <farrel.drmwn00@gmail.com>
2024-09-11 05:30:03 +0700

    Added function to views.py that shows personal details at the footer

--- main/views.py
+++ main/views.py
@@ -3,4 +3,10 @@ from .models import Product  # Import the Product model
 
 def show_main(request):
     products = Product.objects.all()  # Query to get all products
-    return render(request, 'index.html', {'products': products})
+    context = {
+        'products': products,
+        'app_name': 'Free Robux',
+        'name': 'Farrel Dharmawan Dwiyudanto',
+        'class': 'PBP A'
+    }
+    return render(request, 'index.html', context)
